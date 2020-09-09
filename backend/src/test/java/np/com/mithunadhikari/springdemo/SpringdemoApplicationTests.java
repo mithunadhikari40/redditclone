27dd5ebe58eb60e2b2198f533fae2df7a52cf4f8
Backspace Innovation <backspaceinnovation@gmail.com>
2020-09-10 00:06:23 +0545

    the very first commit after setting up the project

---
+++ backend/src/test/java/np/com/mithunadhikari/springdemo/SpringdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package np.com.mithunadhikari.springdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
